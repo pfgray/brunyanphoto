db95e3d9825e6b0f0becda1a80b1fe3fe3ce27c3
Paul Gray <pfbgray@gmail.com>
2016-05-30 15:52:32 -0400

    Adding front/back image switch algorithm

--- src/components/front/Front.js
+++ src/components/front/Front.js
@@ -4,51 +4,72 @@ import Q from 'q';
 import ImageService from '../images/ImageService.js';
 import logo from '../../images/brp_logo_white.png';
 
-const landscapeImages = [];
-const portraitImages = [];
+const FadeDuration = 2000; // ms
+const SwitchInterval = 1000; // ms
+
+const getWallpaperStyleForUrl = (image, text, fading) => {
+  if(!image) {
+    return null;
+  }
+  const style = {
+    opacity: fading ? 0 : 1,
+    transition: fading ? 'opacity ' + FadeDuration + 'ms ease-out' : 'none',
+    backgroundImage: 'url(' + image + ')'
+  };
+  return <div className="wallpaper" style={style}>
+    <h1 style={{ color: 'white' }}>{text}</h1></div>;
+};
 
 const Front = React.createClass({
   getInitialState() {
     return {
-      currentImage: 0
+      frontImage: null,
+      backImage: null,
+      fading: false,
+      landscapeImages: [],
+      portraitImages: []
     };
   },
   componentDidMount() {
+    // render the first two images...
+    // asyncly change the first image...
+    //   set frontImage to fading,
+    //   back Image to fading in
+    // in 5000 ms, switch the photos and repeat
     Q.all([ImageService.getLandscapeAlbum(), ImageService.getPortraitAlbum()])
     .spread((landscape, portrait) => {
       console.log('got albums: ', landscape, portrait);
-
-      portrait.images
-        .map(image => image.link)
-        .forEach(url => portraitImages.push(url));
-      landscape.images
-        .map(image => image.link)
-        .forEach(url => landscapeImages.push(url));
+      this.setState({
+        frontImage: portrait.images.map(i => i.link)[0],
+        backImage: portrait.images.map(i => i.link)[1],
+        landscapeImages: portrait.images.map(i => i.link),
+        portraitImages: landscape.images.map(i => i.link)
+      });
       this.shiftImage();
-      setInterval(this.shiftImage, 5000);
     });
-    // ImageService.getLandscapeImages()
-    //   .then(a => {
-    //
-    //   });
   },
   shiftImage() {
-    console.log('shifting images:', landscapeImages);
-    if(images.length > 0) {
+    // change front Image to fade
+    console.log('shifting image: ', this.state);
+    this.setState({
+      fading: true
+    });
+    setTimeout(() => {
+      console.log('resetting...', this.state);
       this.setState({
-        currentImage: (this.state.currentImage + 1) % images.length
+        frontImage: this.state.backImage,
+        backImage: this.state.frontImage,
+        fading: false
       });
-    }
+      setTimeout(this.shiftImage, SwitchInterval);
+    }, FadeDuration);
   },
   render() {
-    // oh, this is why they all load together...
-    const wallpapers = images.map((image, i) => {
-      const style = {
-        opacity: this.state.currentImage === i ? 1 : 0,
-        backgroundImage: 'url(' + image + ')'
-      };
-      return (<div className="wallpaper" style={style}></div>);
-    });
+
+    const wallpapers = [
+      getWallpaperStyleForUrl(this.state.backImage, 'Back Image'),
+      getWallpaperStyleForUrl(this.state.frontImage, 'Front Image', this.state.fading)
+    ];
 
     return (
       <div className="front">
