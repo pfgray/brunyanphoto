d9e52a4ada9c54b0e0f4b1338f62fc6a91b66b6d
Paul Gray <pfbgray@gmail.com>
2016-09-11 23:36:37 -0400

    adding mobile-specific images

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -1,3 +1,4 @@
+import isMobile from '../detectMobile';
 
 const isPortrait = i => i.height > i.width;
 const isLandscape = i => i.width > i.height;
@@ -11,16 +12,34 @@ const CarouselFactory = {
 
     let landscapeIdx = 0;
     let portraitIdx = 0;
+
+    const getPortraitOrLandscape = function() {
+      const w = Math.max(
+        document.documentElement.clientWidth, window.innerWidth || 0);
+      const h = Math.max(
+        document.documentElement.clientHeight, window.innerHeight || 0);
+      if(w > h) {
+        return landscapeImages[landscapeIdx++ % landscapeImages.length].link;
+      }
+      return portraitImages[portraitIdx++ % portraitImages.length].link;
+    }
+
+    const convertToMobileOrRegular = function(link) {
+      if(isMobile()) {
+        const i = link.lastIndexOf('.');
+        const idAndExt = [link.slice(0, i), link.slice(i + 1)];
+        console.log('parsed:', idAndExt);
+        console.log('joined:', idAndExt.join('l.'));
+        return idAndExt.join('l.');
+      } else {
+        return link;
+      }
+    }
+
     return {
       getNextImage() {
-        const w = Math.max(
-          document.documentElement.clientWidth, window.innerWidth || 0);
-        const h = Math.max(
-          document.documentElement.clientHeight, window.innerHeight || 0);
-        if(w > h) {
-          return landscapeImages[landscapeIdx++ % landscapeImages.length].link;
-        }
-        return portraitImages[portraitIdx++ % portraitImages.length].link;
+        const pic = getPortraitOrLandscape();
+        return convertToMobileOrRegular(pic);
       }
     };
   }
