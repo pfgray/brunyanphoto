67cba03f54411349efdd26ad4440d1c2a2ac802d
Paul Gray <pfbgray@gmail.com>
2017-12-31 11:44:28 -0500

    Fixing email endpoint

--- src/php/functions.php
+++ src/php/functions.php
@@ -4,31 +4,24 @@
   add_theme_support( 'post-formats', array( 'post', 'page', 'gallery' ) );
   // require_once ('gallery-shortcode.php');
 
-  add_action( 'rest_api_init', function () {
-    register_rest_route( 'brunyanphoto/v1', '/email', array(
-      'methods' => 'POST',
-      'callback' => 'email_peeps',
-    ) );
-  } );
-
-
   function email_peeps($request) {
+    error_log('now calling callback  ');
     $data = $request->get_json_params();
-    
+
     $name = $data["name"];
     $email = $data["email"];
     $msg = $data["message"];
     $json = array('name' => $name, 'email' => $email, 'message' => $msg);
-    
+
     $msg = nl2br($msg);
-    
+
     $service_name = "Bethanne Runyan";
     $service_email = "pfbgray@gmail.com";
-    
+
     // To send HTML mail, the Content-type header must be set
     $headers  = 'MIME-Version: 1.0' . "\r\n";
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-    
+
     // Email
     $service_body = "
     <html>
@@ -42,7 +35,7 @@
       </body>
     </html>
     ";
-    
+
     $receipt_body = "
     <html>
       <head>
@@ -57,15 +50,22 @@
       </body>
     </html>
     ";
-    
+
     // mail the client receipt
     $receipt_headers = "To: $name <$email>" . "\r\n" . "From: $service_name <$service_email>" . "\r\n";
     wp_mail($email,"Thanks for contacting Bethanne Runyan Photography", $receipt_body, $headers . $receipt_headers);
-    
+
     // mail the service!
     $service_headers = "To: $service_name <$service_email>" . "\r\n" . "From: $name <$email>" . "\r\n";
     wp_mail($service_email, "Message from: $name ($email)", $service_body, $headers . $service_headers);
-    
+
     return $json;
   }
+
+  add_action( 'rest_api_init', function () {
+    register_rest_route( 'brunyanphoto/v1', 'email', array(
+      'methods' => 'POST',
+      'callback' => 'email_peeps'
+    ) );
+  } );
  ?>
